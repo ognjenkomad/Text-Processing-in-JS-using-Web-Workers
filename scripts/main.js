d7ebba0135eeeb313d6ba340e104fee965576d00
StefanRadovanovic <stefanradovanovic255@gmail.com>
2019-09-22 11:05:12 +0200

    calculate execution time of functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,28 @@
-let stack = [
-  'one',
-  'two',
-  'three',
-  'four',
-  'five'
-]
+const runWorker = (worker, workerName) => {
+	let algorithm = searchAlgorithms.pop();
+	worker.postMessage({ items, algorithm, workerName});
+	worker.onmessage = event => {
+		if (searchAlgorithms.length === 0) {
+			//worker.terminate();
+		} else {
+			algorithm = searchAlgorithms.pop();
+			worker.postMessage({ items, algorithm, workerName});
+		}
+	}
+};
 
-const startTime = new Date().getTime()
+let searchAlgorithms = [
+	'one',
+	'two',
+	'three',
+	'four',
+	'five'
+];
 
-const run = function() {
-  this.postMessage({items: [1, 2, 3, 4], algorithm: stack.pop() })
+let items = [1, 2, 3, 4];
 
-// On finish 
-  this.onmessage = (event) => {
-    console.log(">>>>> returned data", event)
-  if (stack.length === 0) {
-    const endTime = new Date().getTime()
-     console.log("Execution Time: ", Math.round(endTime - startTime) / 1000)
-    // this.terminate() 
-    } else {
-    console.log(">>>>>> new sack", stack);
-    this.postMessage({items: [1, 2, 3, 4], algorithm: stack.pop() })
-  }
-  }
-}
+const worker1 = new Worker('../workers/worker.js?name=Worker1');
+const worker2 = new Worker('../workers/worker.js?name=Worker2');
 
-const worker1 = new Worker('../workers/worker.js?name=Worker1')
-worker1.run = run
-worker1.run()
-const worker2 = new Worker('../workers/worker.js?name=Worker2')
-worker2.run = run
-worker2.run()
+runWorker(worker1, 'worker_1');
+runWorker(worker2, 'worker_2');
--- workers/worker.js
+++ workers/worker.js
@@ -1,70 +1,67 @@
-self.onmessage = event => {  
-  const items = event.data.items
-  const firstAlgorithmOnQueue = event.data.algorithm
+self.onmessage = event => {
+	const workerName = event.data.workerName;
+	const items = event.data.items;
+	const firstAlgorithmOnQueue = event.data.algorithm;
 
-  switch (firstAlgorithmOnQueue) {
-    case 'one':
-      one(items)
-      break
-    case 'two':
-      two(items)
-      break
-    case 'three':
-      three(items)
-      break
-    case 'four':
-      four(items)
-      break
-    case 'five':
-      five(items)
-      break
-  }
-  // Send data to worker onmessage
-  self.postMessage(`>>>>>>>>>>>>>>>>>>>>> FISNISHED ${firstAlgorithmOnQueue}`)
-}
-
+	const startTime = Date.now();
+	switch (firstAlgorithmOnQueue) {
+		case 'one':
+			one(items);
+			break;
+		case 'two':
+			two(items);
+			break;
+		case 'three':
+			three(items);
+			break;
+		case 'four':
+			four(items);
+			break;
+		case 'five':
+			five(items);
+			break;
+	}
+	const duration = (Date.now() - startTime) / 1000;
+	console.log(`Worker finished: function ${firstAlgorithmOnQueue.toUpperCase()} | worker ${workerName.toUpperCase()} | duration: ${duration} s`);
+	self.postMessage(firstAlgorithmOnQueue);
+};
 
 function one(array) {
-  console.log("================= ONE =================")
-  console.log(array.join('>>>>'))
-  let x = 0
-  for(let i= 0; i < 500000000; i++) {
-    x = x + i
-  }
+	console.log('ONE:', array.join('*'));
+	let x = 0;
+	for (let i = 0; i < 700000000; i++) {
+		x = x + i;
+	}
 }
 
 function two(array) {
-  console.log("================= TWO =================")
-  console.log(array.join('||||'))
-  let x = 0
-  for(let i= 0; i < 500000000; i++) {
-    x = x + i
-  }
+	console.log('TWO:', array.join('|'));
+	let x = 0;
+	for (let i = 0; i < 700000000; i++) {
+		x = x + i;
+	}
 }
 
 function three(array) {
-  console.log("================= THREE =================")
-  console.log(array.join('?????'))
-  let x = 0
-  for(let i= 0; i < 500000000; i++) {
-    x = x + i
-  }
+	console.log('THREE:', array.join('.'));
+	let x = 0;
+	for (let i = 0; i < 700000000; i++) {
+		x = x + i;
+	}
 }
 
 function four(array) {
-  console.log("================= FOUR =================")
-  console.log(array.join('----'))
-  let x = 0
-  for(let i= 0; i < 500000000; i++) {
-    x = x + i
-  }
+	console.log('FOUR:', array.join('-'));
+	let x = 0;
+	for (let i = 0; i < 700000000; i++) {
+		x = x + i;
+	}
 }
 
 function five(array) {
-  console.log("================= FIVE =================")
-  console.log(array.join(';;;;'))
-  let x = 0
-  for(let i= 0; i < 500000000; i++) {
-    x = x + i
-  }
+	console.log('FIVE:', array.join(';'));
+	let x = 0;
+	for (let i = 0; i < 700000000; i++) {
+		x = x + i;
+	}
 }
