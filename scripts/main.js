c2a26b7e6650edc27cfa3a30f6fc66f3915b2bcc
Administrator <>
2019-10-12 15:30:27 +0200

    Add Bootstrap Add sequential search

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
-const resultList = document.getElementById('result');
+const resultTableBody = $('#result-table')
+const submitButton = $('#submit-button')
+const searchInput = $('#search-input')
+const processingSelect = $('#processing-select')
+const workerSelectContainer = $('#worker-select-container')
+const workerSelect = $('#worker-select')
+
+
 const students = data.students;
 const searchAlgorithms = [
 		'binary',
@@ -9,24 +16,82 @@ const searchAlgorithms = [
 ];
 let algorithmsQueue = [];
 
+// Events
+submitButton.on('click', findNumber)
+searchInput.on('keypress', (event) => {
+	if(event.which == 13) findNumber()
+})
+
+processingSelect.on('change', (event) => {
+	if (processingSelect.val() == 2) {
+		workerSelectContainer.show()
+	} else {
+		workerSelectContainer.hide()
+	}
+})
+
+
+
 function findNumber() {
 	algorithmsQueue = [...searchAlgorithms];
-	resultList.innerHTML = '';
-	const searchField = document.getElementById('search-value');
-	const searchValue = parseInt(searchField.value);
-	runWorker(worker1, 'worker_1', students, searchValue);
-	runWorker(worker2, 'worker_2', students, searchValue);
+	resultTableBody.find('tbody').empty()
+	const searchValue = parseInt(searchInput.val());
+	if (processingSelect.val() == 2) {
+		numberOfWorkers = workerSelect.val()
+		for (let i = 1; i <= numberOfWorkers; i++) {
+			const worker = new Worker(`../workers/worker.js?name=Worker${i}`);
+			runWorker(worker, `worker_${i}`, students, searchValue);
+		}
+	} else {
+		runAllAlgorithmsSequenital(students, searchValue)
+	}
 }
 
 function logAlgorithmResult(resultData) {
-	let resultMessage = `Algorithm ${resultData.algorithm.toUpperCase()} | ${resultData.workerName.toUpperCase()} | Duration ${resultData.duration} ms | Result: `;
-	if (resultData.result === -1) {
-		resultMessage += `Student is not found at index: ${resultData.searchValue}`;
+	const workerName = resultData.workerName.toUpperCase()
+	const algorithm = resultData.algorithm.toUpperCase()
+	const duration = resultData.duration
+	const result = resultData.result
+	if (result === -1) {
+		appendNoDataFound(workerName, algorithm, duration)
 	} else {
-		let student = resultData.result;
-		resultMessage += `Student is found at index: ${resultData.searchValue}. Name: ${student.firstName} ${student.lastName} Age: ${student.age} Birth Place: ${student.birthPlace}`;
+		appendResultData(workerName, algorithm, duration, result)
 	}
-	resultList.innerHTML += `<li>${resultMessage}</li>`;
+}
+
+const appendNoDataFound = (workerName, algorithm, duration) => {
+	resultTableBody.append(`<tr>
+			<td>${workerName}</td>
+			<td>${algorithm}</td>
+			<td>${duration} ms</td>
+			<td class="text-center" colspan='5'>No results found.</td>
+		</tr>`)
+}
+
+const appendResultData = (workerName, algorithm, duration, result) => {
+		resultTableBody.append(`<tr>
+		<td>${workerName}</td>
+		<td>${algorithm}</td>
+		<td>${duration} ms</td>
+		<td>${result.index}</td>
+		<td>${result.firstName}</td>
+		<td>${result.lastName}</td>
+		<td>${result.birthPlace}</td>
+		<td>${result.age}</td>
+		</tr>`)
+}
+
+const runAllAlgorithmsSequenital = (items, searchValue) => {
+	searchAlgorithms.forEach(algorithm => {
+		const startTime = Date.now()
+		const result = window[`${algorithm}Search`](items, searchValue);
+		const duration = (Date.now() - startTime)
+		if (result === -1) {
+			appendNoDataFound('-', algorithm.toUpperCase(), duration)
+		} else {
+			appendResultData('-', algorithm.toUpperCase(), duration, result)
+		}
+	});
 }
 
 const runWorker = (worker, workerName, items, searchValue) => {
@@ -42,7 +107,4 @@ const runWorker = (worker, workerName, items, searchValue) => {
 		}
 	}
 };
-
-const worker1 = new Worker('../workers/worker.js?name=Worker1');
-const worker2 = new Worker('../workers/worker.js?name=Worker2');
 
